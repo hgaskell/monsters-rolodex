afe0cd18ae47e2c54c22bd860ba6f1793a9468cf
Harry <hgaskell92@hotmail.com>
2020-05-12 17:01:59 +0100

    Updates

---
+++ precache-manifest.4711cdf125f708856c0df16be9f067ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf7d98fe517838b6ffacc7435adf264",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "541fd2126aafd0fae9e8",
+    "url": "/monsters-rolodex/static/css/main.303d26d3.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/monsters-rolodex/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541fd2126aafd0fae9e8",
+    "url": "/monsters-rolodex/static/js/main.d619a17a.chunk.js"
+  },
+  {
+    "revision": "60b3e6aee1ab568e197c",
+    "url": "/monsters-rolodex/static/js/runtime-main.ed829367.js"
+  }
+]);
